7db682bc64b95973bee29351771a088ad1f0db2a
hnjt <18645636597@163.com>
2018-11-07 10:43:56 +0800

    eureka framework of deploy 0.0.1

---
+++ hn_eureka/src/main/java/hn/com/hn_eureka/HnEurekaApplication.java
@@ -0,0 +1,14 @@
+package hn.com.hn_eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class HnEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HnEurekaApplication.class, args);
+    }
+}
